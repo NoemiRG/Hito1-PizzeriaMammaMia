a427065fcc67d10a99670b2f28f0eee4cf65f6f9
NoemiRG <noemirg00@gmail.com>
2026-04-17 01:26:42 -0400

    subida hito

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </>
+    
+      );
+    
+}
+
+export default App
